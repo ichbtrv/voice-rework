c873469e66e00f44bdffa45a60b04813c3b77c51
Ivan C <95117989+ichbtrv@users.noreply.github.com>
2023-01-19 22:42:33 -0800

    Add play button icon

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -10,5 +10,9 @@
   <symbol id="save" width="24" height="24" viewBox="0 0 24 24">
     <path d="M13 3h2.996v5h-2.996v-5zm11 1v20h-24v-24h20l4 4zm-17 5h10v-7h-10v7zm15-4.171l-2.828-2.829h-.172v9h-14v-9h-3v20h20v-17.171z"/>
   </symbol>
+
+  <symbol id="play-button" width="24" height="24" viewBox="0 0 24 24">
+    <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-3 17v-10l9 5.146-9 4.854z" />
+  </symbol>
   <use href="#{name}"/>
 </svg>
