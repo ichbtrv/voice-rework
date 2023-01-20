d29947d485f42016e55386d729e472c374c8f0ee
Ivan C <95117989+ichbtrv@users.noreply.github.com>
2023-01-19 22:40:13 -0800

    Create Icon component

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let name: string;
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg">
+  <symbol id="folder" width="28"height="28" viewBox="0 0 24 24">
+    <path d="M6.083 4c1.38 1.612 2.578 3 4.917 3h11v13h-20v-16h4.083zm.917-2h-7v20h24v-17h-13c-1.629 0-2.305-1.058-4-3z"/>
+  </symbol>
+
+  <use href="#{name}"/>
+</svg>
