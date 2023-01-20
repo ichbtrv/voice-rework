ea75b64c32ec4aab7c816d590499c761fc295cdc
Ivan C <95117989+ichbtrv@users.noreply.github.com>
2023-01-19 22:41:23 -0800

    Add Save icon

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -7,5 +7,8 @@
     <path d="M6.083 4c1.38 1.612 2.578 3 4.917 3h11v13h-20v-16h4.083zm.917-2h-7v20h24v-17h-13c-1.629 0-2.305-1.058-4-3z"/>
   </symbol>
 
+  <symbol id="save" width="24" height="24" viewBox="0 0 24 24">
+    <path d="M13 3h2.996v5h-2.996v-5zm11 1v20h-24v-24h20l4 4zm-17 5h10v-7h-10v7zm15-4.171l-2.828-2.829h-.172v9h-14v-9h-3v20h20v-17.171z"/>
+  </symbol>
   <use href="#{name}"/>
 </svg>
