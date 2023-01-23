de268079551e0460646dd4b1334ba5df1c208942
Ivan C <95117989+ichbtrv@users.noreply.github.com>
2023-01-23 11:03:22 -0800

    Clean up Navbar for ally warnings

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -10,19 +10,27 @@
 
 <section class="flex justify-between">
   <nav>
-    {recording}
     <ul class="flex items-center justify-center">
-      <li class="hover:bg-teal-300 px-2 py-2 rounded-md w-10 h-10" on:click={play}>
+      <li 
+        class="hover:bg-teal-300 px-2 py-2 rounded-md w-10 h-10" 
+        on:click={play}
+        on:keydown={play}
+      >
         {#if playing}
           <Icon name="stop" />
         {:else}
           <Icon name="play" />
         {/if}
       </li>
-      <li class=" {recording
-        ? 'bg-red-500 bg-opacity-10'
-        : 'hover:bg-teal-300'} px-2 py-2 rounded-md w-10 h-10" on:click={record}>
-        <Icon name="record"  fill={recording ? "red" : "black"} />
+      <li 
+        class="px-2 py-2 rounded-md w-10 h-10
+         {recording
+          ? 'bg-red-500 bg-opacity-10'
+          : 'hover:bg-teal-300'}" 
+          on:click={record}
+          on:keydown={record}     
+          >
+        <Icon name="record" fill={recording ? "red" : "black"} />
       </li>
       <li class=" hover:bg-teal-300 px-2 py-2 rounded-md w-10 h-10">
         <Icon name="save" />
