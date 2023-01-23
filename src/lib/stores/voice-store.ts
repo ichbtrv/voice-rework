a725096adae0fe14196a7b77eaae4d5e2a2b14bf
Ivan C <95117989+ichbtrv@users.noreply.github.com>
2023-01-23 12:12:09 -0800

    Update VoiceStore methods

--- src/lib/stores/voice-store.ts
+++ src/lib/stores/voice-store.ts
@@ -1,5 +1,8 @@
-import { writable } from "svelte/store";
+import { get, writable } from "svelte/store";
 import type { Note } from "../../types/note";
+
+let timer; //Timer for total duration
+let counter; //Timer for play counter
 
 export const voiceStore = writable<Note>({
   audio_src: '',
@@ -13,10 +16,34 @@ export const voiceStore = writable<Note>({
   notesVisible : false
 })
 
+function durationStart() {
+  timer = setInterval(() => voiceStore.update(v =>  {return {...v, duration: ++v.duration}}), 1000);
+}
+
+function clearTimers(timerToClear) {
+  clearInterval(timerToClear);
+}
+
+const playStart = () => {
+  counter = setInterval(() => voiceStore.update(v =>  {return {...v, counter: ++v.counter}}), 1000);
+}
+
+
 export const play = () => {
+  const v = get(voiceStore);
+
+  if(v.playing) {
+    document.dispatchEvent(new CustomEvent('pause'));
+    clearTimers(counter);
+    voiceStore.update(vs => {return {...vs, counter: 0}})
+
+  }
+  else playStart(); 
   voiceStore.update(vs => {return {...vs, playing: !vs.playing, stopped: !vs.stopped}})
 }
 
 export const record = () => {
+  if(!get(voiceStore).recording) durationStart();
+  else clearTimers(timer);
   voiceStore.update(vs => {return {...vs, recording: !vs.recording}});
 }
