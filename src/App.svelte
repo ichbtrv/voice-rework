13d76e644c90e8da89706cecadc7c0f9cd743942
Ivan C <95117989+ichbtrv@users.noreply.github.com>
2023-01-23 12:48:18 -0800

    Add Modal and notes view to app

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,29 @@
     import AudioNote from "./lib/AudioNote.svelte";
     import Navbar from "./lib/Navbar.svelte";
     import Recorder from "./lib/Recorder.svelte";
- let count = 1;
+    import Modal from "./lib/Modal.svelte";
+    import Notes from "./lib/Notes.svelte";
+    let count = 1;
+
+    let noteView = false;
+    let modalVisible = false;
 </script>
 
-<main class="flex flex-col justify-center w-[100vw]">
-  <Navbar/>
-  <section class="flex flex-col flex-1 items-center w-[100vw]">
-    <AudioPlayer on:stop />
-    <Recorder />
-  </section>
-  <section>
-    <AudioNote />
-  </section>
+<main class="flex flex-col justify-center w-[100vw] p-4">
+  {#if !noteView}
+    <Navbar 
+      bind:noteView
+      bind:modalVisible/>
+    <section class="flex flex-col flex-1 items-center w-[100vw]">
+      <AudioPlayer />
+      <Recorder />
+    </section>
+    <section>
+      <AudioNote />
+      <Modal bind:modalVisible />
+    </section>
+  {:else}
+    <Notes bind:noteView />
+  {/if}
 </main>
 
