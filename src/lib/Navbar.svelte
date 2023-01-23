6baeb07370f7b3d0283999c521d458835198dd91
Ivan C <95117989+ichbtrv@users.noreply.github.com>
2023-01-23 12:48:43 -0800

    Add functionality to nav to view and save notes

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,18 +1,28 @@
 <script lang="ts">
   import Icon from './Icon.svelte';
-  import {play, record, voiceStore} from './stores/voice-store';
+  import { clearNote, note } from './stores/note';
+  import { clearAudio, play, record, voiceStore } from './stores/voice-store';
+  export let modalVisible;
+  export let noteView;
 
   $: counter = $voiceStore.counter;
   $: duration = $voiceStore.duration;
   $: recording = $voiceStore.recording;
-  $: playing = $voiceStore.playing
+  $: playing = $voiceStore.playing;
+
+  const reset = () => {
+    if(!$note) return;
+
+    clearNote();
+    clearAudio();
+  };
 </script>
 
 <section class="flex justify-between">
   <nav>
     <ul class="flex items-center justify-center">
-      <li 
-        class="hover:bg-teal-300 px-2 py-2 rounded-md w-10 h-10" 
+      <li
+        class="hover:bg-teal-300 px-2 py-2 rounded-md w-10 h-10"
         on:click={play}
         on:keydown={play}
       >
@@ -22,21 +32,34 @@
           <Icon name="play" />
         {/if}
       </li>
-      <li 
+      <li
         class="px-2 py-2 rounded-md w-10 h-10
-         {recording
-          ? 'bg-red-500 bg-opacity-10'
-          : 'hover:bg-teal-300'}" 
-          on:click={record}
-          on:keydown={record}     
-          >
-        <Icon name="record" fill={recording ? "red" : "black"} />
+        {recording ? 'bg-red-500 bg-opacity-10' : 'hover:bg-teal-300'}"
+        on:click={record}
+        on:keydown={record}
+      >
+        <Icon name="record" fill={recording ? 'red' : 'black'} />
       </li>
-      <li class=" hover:bg-teal-300 px-2 py-2 rounded-md w-10 h-10">
+      <li
+        class=" hover:bg-teal-300 px-2 py-2 rounded-md w-10 h-10"
+        on:click={() => (modalVisible = true)}
+        on:keydown={() => (modalVisible = true)}
+      >
         <Icon name="save" />
       </li>
-      <li class="hover:bg-teal-300 px-2 py-1.5 rounded-md w-11 h-10">
+      <li
+        class="hover:bg-teal-300 px-2 py-1.5 rounded-md w-11 h-10"
+        on:click={() => (noteView = true)}
+        on:keydown={() => (noteView = true)}
+      >
         <Icon name="folder" />
+      </li>
+      <li
+        class="hover:bg-teal-300 px-2 py-1.5 rounded-md w-11 h-10"
+        on:click={reset}
+        on:keydown={reset}
+      >
+        <Icon name="trash" />
       </li>
     </ul>
   </nav>
