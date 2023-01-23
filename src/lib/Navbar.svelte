f8b312c9dfb2c5b9adbe12d88ada878be55bfe1b
Ivan C <95117989+ichbtrv@users.noreply.github.com>
2023-01-23 10:48:57 -0800

    Modify navbar to be controlled by store

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
   import Icon from './Icon.svelte';
+  import {play, record, voiceStore} from './stores/voice-store';
 
   let counter = 0;
   let duration = 0;
-  let recording = false;
+  $: recording = $voiceStore.recording;
+  $: playing = $voiceStore.playing
 </script>
 
 <section class="flex justify-between">
   <nav>
+    {recording}
     <ul class="flex items-center justify-center">
-      <li class=" hover:bg-teal-300 px-2 py-2 rounded-md w-10 h-10">
-        <Icon name="play" />
+      <li class="hover:bg-teal-300 px-2 py-2 rounded-md w-10 h-10" on:click={play}>
+        {#if playing}
+          <Icon name="stop" />
+        {:else}
+          <Icon name="play" />
+        {/if}
       </li>
-      <li class=" hover:bg-teal-300 px-2 py-2 rounded-md w-10 h-10">
-        <Icon name="record" />
+      <li class=" {recording
+        ? 'bg-red-500 bg-opacity-10'
+        : 'hover:bg-teal-300'} px-2 py-2 rounded-md w-10 h-10" on:click={record}>
+        <Icon name="record"  fill={recording ? "red" : "black"} />
       </li>
       <li class=" hover:bg-teal-300 px-2 py-2 rounded-md w-10 h-10">
         <Icon name="save" />
