29e0feb04521adcb7af050505d412ab77633fa4f
Ivan C <95117989+ichbtrv@users.noreply.github.com>
2023-01-23 10:35:00 -0800

    Modify App to display Navbar + placeholder

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+    import Navbar from "./lib/Navbar.svelte";
  let count = 1;
 </script>
 
-<main>
-{count}
+<main class="flex flex-col justify-center w-[100vw]">
+  <Navbar/>
+  <section class="flex flex-col flex-1 items-center w-[100vw]">
+    hello
+  </section>
 </main>
 
