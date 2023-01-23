121bf285b72e3d76ea1c68d625a9c5208c61dc52
Ivan C <95117989+ichbtrv@users.noreply.github.com>
2023-01-23 12:12:43 -0800

    Update App to display recorder and note

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
+    import AudioPlayer from "./lib/AudioPlayer.svelte";
+    import AudioNote from "./lib/AudioNote.svelte";
     import Navbar from "./lib/Navbar.svelte";
+    import Recorder from "./lib/Recorder.svelte";
  let count = 1;
 </script>
 
 <main class="flex flex-col justify-center w-[100vw]">
   <Navbar/>
   <section class="flex flex-col flex-1 items-center w-[100vw]">
-    hello
+    <AudioPlayer on:stop />
+    <Recorder />
+  </section>
+  <section>
+    <AudioNote />
   </section>
 </main>
 
