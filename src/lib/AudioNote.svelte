1bad6e4f1c6001a52d8c5d576c156204b475eb2c
Ivan C <95117989+ichbtrv@users.noreply.github.com>
2023-01-23 11:02:58 -0800

    Create AudioNote component

---
+++ src/lib/AudioNote.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { note } from "./stores/note";
+
+  $: noteContent = $note;
+</script>
+
+<section class="mt-8 flex justify-center">
+  <p
+    class="text-center text-teal-300 text-2xl mt-2 p-2 word-wrap max-w-[50vw]"
+    bind:textContent={noteContent}
+    contenteditable
+  />
+</section>
