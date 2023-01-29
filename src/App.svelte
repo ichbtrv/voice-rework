4ee08155bf5c36e42b54072db4a8ba7257fd7149
Ivan C <95117989+ichbtrv@users.noreply.github.com>
2023-01-29 10:07:04 -0800

    Add recorder to App

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,31 @@
   import AudioPlayer from './lib/AudioPlayer.svelte';
   import AudioNote from './lib/AudioNote.svelte';
   import Navbar from './lib/Navbar.svelte';
-  import Recorder from './lib/Recorder.svelte';
   import Modal from './lib/Modal.svelte';
   import Notes from './lib/Notes.svelte';
+  import { recorder } from'./lib/utils/recorder';
+  import { onMount } from 'svelte';
+  import { voiceStore } from './lib/stores/voice-store';
 
   let noteView = false;
   let modalVisible = false;
+
+  $: recording = $voiceStore.recording;
+  $: recordingStarted = false;
+  
+  onMount(async() => {
+    await recorder();
+  })
+
+  $: if(recording) {
+    recordingStarted = true;
+    document.dispatchEvent(new CustomEvent('recording:start'));
+  }
+
+  $: if(!recording && recordingStarted) {
+    document.dispatchEvent(new CustomEvent('recording:stop'));
+    recordingStarted = false;
+  }
 </script>
 
 <main class="flex flex-col justify-center w-[100vw] p-4">
@@ -15,7 +34,6 @@
     <Navbar bind:noteView bind:modalVisible />
     <section class="flex flex-col flex-1 items-center w-[100vw]">
       <AudioPlayer />
-      <Recorder />
     </section>
     <section>
       <AudioNote />
